11d3eadf34c9d87526fd51f9f8cb2d4f6ee3d429
Ling Song <lingsong.c4@gmail.com>
2026-09-01 20:06:08 +0800

    Expand GUI to have basic UI

---
+++ src/main/java/soar/DialogBox.java
@@ -0,0 +1,34 @@
+package soar;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+/**
+ * Displays a message together with its author's image.
+ */
+public class DialogBox extends HBox {
+
+    private final Label text;
+    private final ImageView displayPicture;
+
+    /**
+     * Creates a dialog box containing the specified text and image.
+     *
+     * @param message Message to display.
+     * @param image Image displayed beside the message.
+     */
+    public DialogBox(String message, Image image) {
+        text = new Label(message);
+        displayPicture = new ImageView(image);
+
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        setAlignment(Pos.TOP_RIGHT);
+
+        getChildren().addAll(text, displayPicture);
+    }
+}
